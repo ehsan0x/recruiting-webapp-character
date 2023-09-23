6c131262e67c1bb93aa251658f34e1c35b52bf99
ehsan0x <emamakani@gmail.com>
2023-09-23 16:32:46 -0400

    Helper functions

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,1 +1,9 @@
 export const calculateModifier = (value) => Math.floor((value - 10) / 2);
+
+export const isValidValue = (value) => {
+  const valid = value >= 0;
+  console.log(`isValidValue called with ${value}. Result: ${valid}`);
+  return valid;
+};
+
+export const alertUser = (message) => window.alert(message);
